79de2d6e6572d3728d5991a3fe78ac12b074bf4c
Ashish <50015357+AshishThakur10@users.noreply.github.com>
2020-03-19 15:33:26 +0530

    Updated GameController
    Added Speed of the snake

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,6 +6,7 @@ public class GameController : MonoBehaviour
    
 {
     public static GameController instance = null;
+    public float snakeSpeed = 1; 
     // Start is called before the first frame update
     void Start()
     {
